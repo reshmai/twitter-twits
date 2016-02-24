bcc06d3acc0f18524ec2fb550f8ac885642b76bf
Reshma Ingavale <reshma.ingavale@clariontechnologies.co.in>
2016-02-24 18:12:48 +0530

    added openshift configuration

--- config/config.php
+++ config/config.php
@@ -10,13 +10,16 @@ class Database {
     private function __clone() {}
 
     public static function getInstance() {
+      if (array_key_exists('OPENSHIFT_APP_NAME', $_SERVER)) {
+        $src = $_SERVER;
+      } else {
+        $src = $_ENV;
+      }
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-<<<<<<< HEAD
-        self::$instance = new PDO('mysql:host=localhost;dbname=refer', 'root', 'clarion', $pdo_options);
-=======
-        self::$instance = new PDO('mysql:host=localhost;dbname=refer', 'root', 'poiuytrewq', $pdo_options);
->>>>>>> 664d99d1a457edb046edc294f4f54260278744ae
+
+        self::$instance = new PDO('mysql:host='.$src['OPENSHIFT_MYSQL_DB_HOST'].';dbname='.$src['OPENSHIFT_APP_NAME'], $src['OPENSHIFT_MYSQL_DB_USERNAME'], $src['OPENSHIFT_MYSQL_DB_PASSWORD'], $pdo_options);
+
       }
       return self::$instance;
     }
