3ea84716bba88981b0d71c6ac6a9faf6c9ea5b68
Reshma Ingavale <reshma.ingavale@clariontechnologies.co.in>
2016-02-24 16:45:43 +0530

    done with the connection adding in config.php file

--- config/config.php
+++ config/config.php
@@ -2,6 +2,7 @@
 /*
 * Mysql database class - only one connection alowed
 */
+
 class Database {
 	private static $instance = NULL;
 
@@ -10,9 +11,14 @@ class Database {
     private function __clone() {}
 
     public static function getInstance() {
+      if (array_key_exists('OPENSHIFT_APP_NAME', $_SERVER)) {
+        $src = $_SERVER;
+      } else {
+        $src = $_ENV;
+      }
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=refer', 'root', 'clarion', $pdo_options);
+        self::$instance = new PDO('mysql:host='.$src['OPENSHIFT_MYSQL_DB_HOST'].';dbname='.$src['OPENSHIFT_APP_NAME'], $src['OPENSHIFT_MYSQL_DB_USERNAME'], $src['OPENSHIFT_MYSQL_DB_PASSWORD'], $pdo_options);
       }
       return self::$instance;
     }
