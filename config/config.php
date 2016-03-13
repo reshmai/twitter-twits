087b5fcb606591e66ee7566c948b15b62c44514f
Rana Ranvijay Singh <ranaranvijaysingh9@gmail.com>
2016-03-13 21:24:32 +0530

    config changed

--- config/config.php
+++ config/config.php
@@ -13,16 +13,16 @@ class Database {
       if (!isset(self::$instance)) {
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
         
-        // define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-        // define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
-        // define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-        // define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-        // define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+        define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+        define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+        define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+        define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+        define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
 
-        // $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
-        // self::$instance = new PDO($dsn, DB_USER, DB_PASS);
+        $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+        self::$instance = new PDO($dsn, DB_USER, DB_PASS);
         
-        self::$instance = new PDO('mysql:host=localhost;dbname=refer', 'root', 'poiuytrewq', $pdo_options);
+        //self::$instance = new PDO('mysql:host=localhost;dbname=refer', 'root', 'poiuytrewq', $pdo_options);
 
       }
       return self::$instance;
