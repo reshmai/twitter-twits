3354e5f2ab2346a12c78124a54315241c7ad88a6
Rana Ranvijay Singh <ranaranvijaysingh9@gmail.com>
2016-03-13 21:27:08 +0530

    config changed

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
 define('PUBLIC_PATH',dirname(realpath(__FILE__)) . "/");
 define('BASE_PATH',dirname(PUBLIC_PATH));
